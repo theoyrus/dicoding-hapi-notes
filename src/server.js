0831d22b84c8d9c2ef0f0eac5f56b919c9fb05a3
Suryo Prasetyo W <the.oyrus@gmail.com>
2021-06-20 16:49:17 +0700

    add: postgresql database connectivity

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 // const routes = require('./routes');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+// const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
+    port: process.env.PORT,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
